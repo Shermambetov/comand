d5dde00f45038c6919fa7b005fc6eb6ba19b0d12
Jazmira <ujazmira@gmail.com>
2023-02-28 19:10:58 +0600

    jazmira added

--- main.js
+++ main.js
@@ -5,17 +5,39 @@ let API = "http://localhost:8000/posts";
 let imgInp = document.querySelector("#image");
 let titleInp = document.querySelector("#title");
 let descrInp = document.querySelector("#descr");
-let navModal = document.querySelector(".main navbar");
-let btnAddPost = document.querySelector(".btn-add-post");
-let contentPost = document.querySelector(".content");
+let navModal = document.querySelector("#navbar");
+
+let saveBtn = document.querySelector("#btn-save-post");
+let contentPost = document.querySelector("#content");
+let contentModal = document.querySelector("#open-post");
+let btnEdit = document.querySelector("#btn-edit");
+let btnDelete = document.querySelector("#btn-delete");
+// let btnModal = document.querySelector("#")
+
 // console.log(imgInp, titleInp, descrInp, navModal, btnAddPost);
+// ? кнопки для эдита
+let editImgInp = document.querySelector("#edit-image");
+let editTitleInp = document.querySelector("#edit-title");
+let editDescrInp = document.querySelector("#edit-descr");
+let editBtnSave = document.querySelector("#btn-save-edit");
+let exampleModal = document.querySelector("#exampleModal");
+// ? погинация
+let paginationList = document.querySelector(".pagination-list");
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
+let page = 1;
+let totalPageCount = 1;
+// ?search
+let searchInp = document.querySelector("#search");
+let searchVal = "";
 let newPost = "";
-btnAddPost.addEventListener("click", async function () {
+saveBtn.addEventListener("click", async function () {
   let post = {
     image: imgInp.value,
     title: titleInp.value,
     description: descrInp.value,
   };
+  //   console.log(post)
   if (!post.image.trim() || !post.title.trim() || !post.description.trim()) {
     alert("Заполните поля");
     return;
@@ -32,33 +54,138 @@ btnAddPost.addEventListener("click", async function () {
   descrInp.value = "";
   render();
 });
-// ? функция для отображения карточек
+
 async function render() {
-  let res = await fetch(API);
+  let res = await fetch(`${API}?q=${searchVal}&_page=${page}&_limit=3`);
   let posts = await res.json();
+  paginationButton();
   contentPost.innerHTML = "";
   posts.forEach((e) => {
     let newElem = document.createElement("div");
     newElem.id = e.id;
     newElem.innerHTML = `<div class="card" style="width: 18rem;">
-      <img src="${e.image}" class="card-img-top" alt="...">
-      <div class="card-body">
+        <img src="${e.image}" class="card-img-top" alt="...">
+        <div class="card-body">
         <h5 class="card-title">${e.title}</h5>
         <p class="card-text">${e.description}</p>
-         <a href="#" class="btn btn-danger btn-delete">Delete</a>
-         <a href="#" class="btn btn-primary btn-edit">Edit</a>
-      </div>
-    </div>`;
+        <a href="#" id=${e.id} class="btn btn-danger btn-delete">Delete</a>
+        <a href="#" id="${e.id}" class="btn btn-primary btn-edit" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</a>
+        </div>
+        </div>`;
     contentPost.append(newElem);
   });
 }
-render();
 
-document.addEventListener("click", async (elem) => {
-  if (elem.target.classlist.contains("btn-delete")) {
-    console.log("delete clicked");
-    let id = elem.target.id;
-    await fetch(`${API}/${id}`, { method: "DELETE" });
+render();
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-delete")) {
+    let id = e.target.id;
+    fetch(`${API}/${id}`, { method: "DELETE" }).then(() => render());
+  }
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("btn-edit")) {
+    let id = e.target.id;
+    fetch(`${API}/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        editImgInp.value = data.imgInp;
+        editTitleInp.value = data.titleInp;
+        editDescrInp.value = data.descrInp;
+        editBtnSave.setAttribute("id", data.id);
+      });
+  }
+});
+
+editBtnSave.addEventListener("click", function () {
+  let id = this.id;
+  //   console.log(id);
+  imgInp = editImgInp.value;
+  titleInp = editTitleInp.value;
+  descrInp = editDescrInp.value;
+
+  if (!imgInp || !titleInp || !descrInp) {
+    alert("Заполните поля");
+    return;
+  }
+  let editedPost = {
+    imgInp,
+    titleInp,
+    descrInp,
+  };
+  saveEdit(editedPost, id);
+
+  //   console.log(modal);
+});
+
+function saveEdit(editedPost, id) {
+  fetch(`${API}/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify(editedPost),
+  }).then(() => render());
+  let modal = bootstrap.Modal.getInstance(exampleModal);
+  modal.hide();
+}
+
+function paginationButton() {
+  fetch(`${API}?q=${searchVal}`)
+    .then((res) => res.json())
+    .then((data) => {
+      totalPageCount = Math.ceil(data.length / 3);
+      paginationList.innerHTML = "";
+      for (let i = 1; i <= totalPageCount; i++) {
+        if (page == i) {
+          let page1 = document.createElement("li");
+          page1.innerHTML = `<li class="page-item active"><a class="page-link" href="#">${i}</a></li>`;
+          paginationList.append(page1);
+        } else {
+          let page1 = document.createElement("li");
+          page1.innerHTML = `<li class="page-item"><a class="page-link" href="#">${i}</a></li>`;
+          paginationList.append(page1);
+        }
+      }
+      if (page == 1) {
+        prev.classList.add("disabled");
+      } else {
+        prev.classList.add("disabled");
+      }
+
+      if (page == totalPageCount) {
+        next.classList.add("disabled");
+      } else {
+        next.classList.add("disabled");
+      }
+    });
+}
+prev.addEventListener("click", () => {
+  if (page <= 1) {
+    return;
+  }
+  page--;
+  render();
+});
+
+next.addEventListener("click", () => {
+  if (page >= totalPageCount) {
+    return;
+  }
+  page++;
+  render();
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("page_namber")) {
+    console.log("pagination number clicked");
+    page = e.target.innerText;
     render();
   }
+});
+
+searchInp.addEventListener("input", () => {
+  searchVal = searchInp.value;
+  render();
 });
